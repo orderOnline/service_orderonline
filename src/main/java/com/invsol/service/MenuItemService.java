14536bc9abbc158422e653374454234bd4351f25
rachnakhokhar <rachnakhokhar@gmail.com>
2014-10-07 14:13:57 +0530

    Code integration for Categories, MenuItems, ForgotPassword, Profile, ResetPassword

--- src/main/java/com/invsol/service/MenuItemService.java
+++ src/main/java/com/invsol/service/MenuItemService.java
@@ -1,5 +1,74 @@
 package com.invsol.service;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("menuitem")
 public class MenuItemService {
+	
+	@PUT
+	@Path("/{id}.json")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response updateMenuItem(@PathParam("id") String restaurantID, InputStream incomingData) {
+ 
+		StringBuilder crunchifyBuilder = new StringBuilder();
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(incomingData));
+            String line = null;
+            while ((line = in.readLine()) != null) {
+                crunchifyBuilder.append(line);
+            }
+        } catch (Exception e) {
+            System.out.println("Error Parsing: - ");
+        }
+        System.out.println("Profile Data Received: " + crunchifyBuilder.toString());
+ 
+        // return HTTP response 200 in case of success
+        return Response.status(200).entity(crunchifyBuilder.toString()).build();
+ 
+	}
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response addMenuItem(InputStream incomingData) {
+ 
+		StringBuilder crunchifyBuilder = new StringBuilder();
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(incomingData));
+            String line = null;
+            while ((line = in.readLine()) != null) {
+                crunchifyBuilder.append(line);
+            }
+        } catch (Exception e) {
+            System.out.println("Error Parsing: - ");
+        }
+        System.out.println("Authenticate Data Received: " + crunchifyBuilder.toString());
+ 
+        // return HTTP response 200 in case of success
+        return Response.status(200).entity(crunchifyBuilder.toString()).build();
+ 
+	}
+	
+	@DELETE
+	@Path("/{id}.json")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String deleteMenuItem(@PathParam("id") String menuitemID) {
+ 
+		return ("menuitemID is=="+menuitemID);
+        
+	}
 
 }
--- src/main/java/com/invsol/service/ProfileService.java
+++ src/main/java/com/invsol/service/ProfileService.java
@@ -1,5 +1,9 @@
 package com.invsol.service;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
@@ -8,8 +12,6 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
-import com.invsol.model.Sample;
 
 @Path("profile")
 public class ProfileService {
@@ -26,10 +28,22 @@ public class ProfileService {
 	@PUT
 	@Path("/business/{id}.json")
 	@Consumes(MediaType.APPLICATION_JSON)
-	public Response createTrackInJSON(Sample track) {
+	public Response updateBusinessUserProfile(@PathParam("id") String restaurantID, InputStream incomingData) {
  
-		String result = "Track saved : " + track;
-		return Response.status(201).entity(result).build();
+		StringBuilder crunchifyBuilder = new StringBuilder();
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(incomingData));
+            String line = null;
+            while ((line = in.readLine()) != null) {
+                crunchifyBuilder.append(line);
+            }
+        } catch (Exception e) {
+            System.out.println("Error Parsing: - ");
+        }
+        System.out.println("Profile Data Received: " + crunchifyBuilder.toString());
+ 
+        // return HTTP response 200 in case of success
+        return Response.status(200).entity(crunchifyBuilder.toString()).build();
  
 	}
 	
